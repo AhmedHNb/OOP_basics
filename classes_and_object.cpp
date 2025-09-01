9f0490706ba7c8e8c763648d8ead737da299029b
Ahmed Alnabelsi <ahmadnb71@gmail.com>
2025-09-01 14:35:21 +0300

    declspec

--- classes_and_object.cpp
+++ classes_and_object.cpp
@@ -5,35 +5,37 @@ class person
 {
 
     private :
-        string first_name ;
-        string last_name ;
+        string _first_name ;
+        string _last_name ;
 
     public :
 
         void set_first_name (string f_name)
         {
-            first_name = f_name ;
+            _first_name = f_name ;
         }
 
         void set_last_name (string l_name)
         {
-            last_name = l_name ;
+            _last_name = l_name ;
         }
 
         string get_full_name ()
         {
-            return first_name + " " + last_name ;
+            return _first_name + " " + _last_name ;
         }    
 
         string get_first_name ()
         {
-            return first_name ;
+            return _first_name ;
         }
 
         string get_last_name ()
         {
-            return last_name ;
+            return _last_name ;
         }
+
+    __declspec(property(get = get_first_name , put = set_first_name)) string first_name ;
 };
 
 int main ()
@@ -41,7 +43,7 @@ int main ()
     person p1 ;
     p1.set_first_name("ahmed");
     p1.set_last_name("alnabelsi");
-    cout << p1.get_first_name() << endl ;
+    cout << p1.first_name << endl ;
     cout << p1.get_last_name() << endl ;
     cout << p1.get_full_name() << endl ;
 }
